e48105805d91bb35aa894e3b44d2a70094b20980
Justin <jleew357@gmail.com>
2019-09-12 01:22:26 +0000

    lesson 12 stillproblem

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,9 +1,14 @@
 class PlacesController < ApplicationController
 
-  def index
-    #@places = Place.all
-    @places = @Place.paginate(:page => params [:page], :per_page=>5)
+  def places
+    @places = Place.all
+    #@places = @Place.paginate(:page => params [:page], :per_page=>5)
+    
+    #@places = Place.order("name").paginate
+    #places = Place.order("name").page(params[:page])
+end
 
-  end
+
+  
 
 end
