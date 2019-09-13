7ef64b6c0733c2d4de100f659b109dfd943a5a46
Justin <jleew357@gmail.com>
2019-09-13 00:38:50 +0000

    lesson 12 back to beginning

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,8 +1,8 @@
 class PlacesController < ApplicationController
 
-  def places
+  def index
     @places = Place.all
-    #@places = @Place.paginate(:page => params [:page], :per_page=>5)
+    #@places = @Place.paginate(:page => params [:page], :per_page=>3)
     
     #@places = Place.order("name").paginate
     #places = Place.order("name").page(params[:page])
