e3d085dadc37f2abbb15c24b85bd182b67a2d2a6
Justin <jleew357@gmail.com>
2019-09-13 01:32:05 +0000

    lesson 12 more places

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -3,7 +3,7 @@ class PlacesController < ApplicationController
   def index
     @places = Place.all
     #@places = @Place.paginate(:page => params [:page], :per_page=>3)
-    
+    #@places = place.paginate(:page => params[:page], :per_page => 2)
     #@places = Place.order("name").paginate
     #places = Place.order("name").page(params[:page])
 end
