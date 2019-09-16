8a1251fc26d5a80d74fde3ce9db5c3ca4c23fa1c
Justin <jleew357@gmail.com>
2019-09-16 01:13:14 +0000

    lesson 14

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -9,11 +9,22 @@ class PlacesController < ApplicationController
     #@places = Place.all(params[:term], params[:page])
   
 
-end
+  end
 
   def new
     @place = Place.new
   end
   
+  def create
+    Place.create(place_params)
+    redirect_to root_path
+  end
+
+  private
+
+  def place_params
+    params.require(:place).permit(:name, :description, :address)
+  end
+
 
 end
