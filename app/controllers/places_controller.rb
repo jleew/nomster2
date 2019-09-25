687836ea61c470f1905f887e6d6979f24a197e27
Justin <jleew357@gmail.com>
2019-09-25 01:03:57 +0000

    L23 added update to edit

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -30,9 +30,9 @@ class PlacesController < ApplicationController
   end
 
   def update
-    @place = Place.find(params[:id])
-    @place.update_attributes(place_params)
-    redirect_to root_path
+  @place = Place.find(params[:id])
+  @place.update_attributes(place_params)
+  redirect_to root_path
   end
 
   private
