882ede0aee7c7dd57717316c8557c694f1f2dfc4
Justin <jleew357@gmail.com>
2019-09-26 01:36:55 +0000

    L27 Made valdation so a place must have a name

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -17,8 +17,12 @@ class PlacesController < ApplicationController
   end
   
   def create
-    current_user.places.create(place_params)
-    redirect_to root_path
+    @place = current_user.places.create(place_params)
+    if @place.valid?
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def show
@@ -33,14 +37,18 @@ class PlacesController < ApplicationController
     end 
   end
 
-  def update
+def update
   @place = Place.find(params[:id])
   if @place.user != current_user
     return render plain: 'Not Allowed', status: :forbidden
   end
   @place.update_attributes(place_params)
-  redirect_to root_path
+  if @place.valid?
+    redirect_to root_path
+  else
+    render :edit, status: :unprocessable_entity
   end
+end
 
   def destroy
   @place = Place.find(params[:id])
