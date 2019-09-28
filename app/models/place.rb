8d7d6857dda4e55d9534e0e35cd143d9047eabea
Justin <jleew357@gmail.com>
2019-09-28 01:10:38 +0000

    L29 still need to validate. but geocoder is working

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,4 +1,12 @@
 class Place < ApplicationRecord
   belongs_to :user
+
+  geocoded_by :address 
+  after_validation :geocode
+  
+
   validates :name, presence: true
+  #validates :address, pressence: true
+  #validates :description, pressence: true
+  #validates :name=3>, pressence: true
 end
