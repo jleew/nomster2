d6b775c98ac9aea4587a14e0f25db3d93201716c
Justin <jleew357@gmail.com>
2019-10-01 00:45:43 +0000

    added comments somehow

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -5,9 +5,7 @@ class PlacesController < ApplicationController
     #@places = Place.all
     @places = Place.paginate(page: params[:page], per_page: 3)
     #@places = place.paginate(:page => params[:page], :per_page => 2)
-    #@places = Place.order("name").paginate
-    #places = Place.order("name").page(params[:page])
-    #@places = Place.all(params[:term], params[:page])
+    
   
 
   end
@@ -27,6 +25,7 @@ class PlacesController < ApplicationController
 
   def show
     @place = Place.find(params[:id])
+    @comment = Comment.new
   end
 
   def edit
