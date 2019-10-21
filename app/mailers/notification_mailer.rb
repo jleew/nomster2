7d62dc2e4cb382f8c1078ebb843c9177dcc34a83
Justin <jleew357@gmail.com>
2019-10-21 14:01:47 +0000

    Add mailer gmail mail

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -2,7 +2,7 @@ class NotificationMailer < ApplicationMailer
   default from: "no-reply@nomster2app.com"
 
   def comment_added
-    mail(to: "jleew357@email.com",
+    mail(to: "jleew357@gmail.com",
       subject: "A comment has been added to your place")
   end
 end
