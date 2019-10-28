a1c9fc6ba532147cf4377c593f76756f4f56c452
Justin <jleew357@gmail.com>
2019-10-28 14:00:26 +0000

    trying to work out the db

--- db/migrate/20191014140834_add_caption_field_to_photos.rb
+++ db/migrate/20191014140834_add_caption_field_to_photos.rb
@@ -1,16 +1,6 @@
 class AddCaptionFieldToPhotos < ActiveRecord::Migration[5.2]
   def change
-    create_table :photos do |t|
-      t.text :caption
-      
-      
-      t.integer :place_id
-
-      t.timestamps
-    end
-
-    add_index :comments, [:user_id, :place_id]
-    add_index :comments, :place_id
+    
     
     add_column :photos, :caption, :text
     add_column :photos, :place_id, :integer
