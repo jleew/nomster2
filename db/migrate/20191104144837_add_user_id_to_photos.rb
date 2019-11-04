42d01f1b7617da76aea0de74d2dfe8a972e293fd
Justin <jleew357@gmail.com>
2019-11-04 15:10:13 +0000

    Added photos

---
+++ db/migrate/20191104144837_add_user_id_to_photos.rb
@@ -0,0 +1,8 @@
+class AddUserIdToPhotos < ActiveRecord::Migration[5.2]
+  def change
+    add_column :photos, :user_id, :integer
+    add_index :photos, [:user_id, :place_id]
+  end
+
+
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_14_230231) do
+ActiveRecord::Schema.define(version: 2019_11_04_144837) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,8 @@ ActiveRecord::Schema.define(version: 2019_10_14_230231) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "picture"
+    t.integer "user_id"
+    t.index ["user_id", "place_id"], name: "index_photos_on_user_id_and_place_id"
   end
 
   create_table "places", force: :cascade do |t|
